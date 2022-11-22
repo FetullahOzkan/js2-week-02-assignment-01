933c66cc15e9f3c09b5d6d928746acd0b629cee7
FetullahOzkan <111522168+FetullahOzkan@users.noreply.github.com>
2022-11-22 17:39:51 +0100

    changes has done

--- functions.js
+++ functions.js
@@ -1,10 +1,27 @@
-const findTheNumberOfOddNumbers = () => {};
+const findTheNumberOfOddNumbers = (pArray) => {
+  const odds = pArray.filter((num) => num % 2 == 1);
+console.log(odds.length); 
+};
+findTheNumberOfOddNumbers([3, 6, 7, 8, 4, 2]);
 
-const getIndexOfFalseItem = () => {};
+const getIndexOfFalseItem = (pArray) => {
+  const falseItem = pArray.findIndex(item => item == false);
+  console.log(falseItem)
+};
+getIndexOfFalseItem([45, 'Hi', false, true, 23, 'Coders']);
 
-const filterNumbers = () => {};
 
-const generateEmailAddress = () => {};
+const filterNumbers = (pArray) => {
+  const result = pArray.filter(item => typeof (item) === 'number');
+  console.log(result[0])
+};
+filterNumbers([false, 'Hi', true, 23, 'false', 55])
+
+const generateEmailAddress = (pArray) => {
+  const result = pArray.map(item => item.toLowerCase()+"@gmail.com");
+  console.log(result[0])
+}
+generateEmailAddress(["Huseyin", "Ahmet", "Mehmet"])
 
 export {
   findTheNumberOfOddNumbers,
